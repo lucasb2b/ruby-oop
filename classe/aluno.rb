55f92dadbda2e1008cacd1a8ef9fbc8a7a691f8d
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-07-26 23:21:36 -0300

    Classes e Objetos

--- classe/aluno.rb
+++ classe/aluno.rb
@@ -1,11 +1,32 @@
 class Aluno
   attr_accessor :nome, :idade
+  @last_name
+  # Somente Leitura
+  # attr_reader :nome, :idade
+  # Somente Escrita
+  # attr_writer :nome, :idade
+
+  # Método construtor
+  def initialize(nome, idade)
+    @nome = nome
+    @idade = idade
+  end
+
+  def mostrar_dados
+    puts "Nome: #{self.nome} #{@last_name}, Idade: #{@idade}"
+  end
+
+  def add_last_name(last_name)
+    @last_name = last_name
+  end
 end
 
-a1 = Aluno.new
+a1 = Aluno.new("Lucas", 23)
+a1.add_last_name("Brito")
 
-a1.nome = "Diego"
-a1.idade = 35
+a1.mostrar_dados
+# a1.nome = "Diego"
+# a1.idade = 35
 
 puts a1.nome
 puts a1.idade
