5d7bd7bf2e61892d3824d504c7034d7188faf4a9
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-07-27 21:32:02 -0300

    Composição

---
+++ turma/turma.rb
@@ -0,0 +1,20 @@
+class Turma
+  attr_reader :alunos, :professor
+
+  def initialize(alunos, professor)
+    @alunos = Array.new
+    @professor = professor
+  end
+
+  def dados_turma
+    puts "Disciplina: #{professor.disciplina}, Professor(a) Responsável: #{professor.nome}"
+    listar_alunos
+  end
+
+  private
+  def listar_alunos
+    @alunos.each do |aluno|
+      puts "Nome: #{aluno.nome}, Matricula: #{aluno.matricula}"
+    end
+  end
+end
